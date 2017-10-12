ada3d7b5373bebdf642e7091ff71c5c83f362172
Cody Hahn <codhah92@gmail.com>
2017-10-12 11:14:41 -0400

    Update cron service periodic config and views

---
+++ views/reactapp/manifests/add.manifest.js
@@ -0,0 +1,124 @@
+'use strict';
+const path = require('path');
+
+module.exports = (periodic) => {
+  // let reactadmin = periodic.app.controller.extension.reactadmin;
+
+  return {
+    containers: {
+      '/extension/cron/add': {
+        'layout': {
+          'component': 'Hero',
+          'props': {
+            'size': 'isFullheight',
+          },
+          'children': [{
+            'component': 'HeroBody',
+            'props': {},
+            'children': [{
+              'component': 'Container',
+              'props': {
+                style: {
+                  paddingTop: '50px'
+                }
+              },
+              'children': [{
+                'component': 'Columns',
+                'children': [{
+                  'component': 'Column',
+                  'props': {
+                    'size': 'is1',
+                  },
+                },
+                {
+                  'component': 'Column',
+                  'props': {},
+                  'children': [{
+                    'component': 'Title',
+                    'props': {
+                      'style': {
+                        'textAlign': 'center',
+                      },
+                    },
+                    'children': 'Add Cron',
+                  }, 
+                  {
+                    component: 'ResponsiveCard',
+                    children: [
+                      {
+                        component: 'ResponsiveForm',
+                        props: {
+                          onSubmit:{
+                            url:'/crons?format=json&unflatten=true&updateprofile=true&updatecallback=true&handleupload=true&forcequerytobody=true&encryptfiles=true',
+                            options:{
+                              method:'POST',
+                            },
+                            success: true,
+                            successCallback: 'func:this.props.reduxRouter.push',
+                          },
+                          flattenFormData: true,
+                          footergroups: false,
+                          formgroups:[
+                            // {
+                            //   formElements: [
+                            //     {
+                            //       label: 'Name',
+                            //       name: 'name'
+                            //     },
+                            //     {
+                            //       label: 'Theme',
+                            //       name: 'theme'
+                            //     },
+                            //   ],
+                            // },
+                            {
+                              formElements: [
+                                {
+                                  label: 'Interval',
+                                  name: 'cron_interval'
+                                },
+                              ],
+                            },
+                            {
+                              formElements: [
+                                {
+                                  type: 'file',
+                                  label: 'Upload Script',
+                                  name: 'cron_file'
+                                },
+                              ],
+                            },
+                            {
+                              formElements:[
+                                {
+                                  type:'submit',
+                                  value:'Save',
+                                },
+                              ],
+                            },
+                          ],
+                        },
+                      },
+                    ],
+                  },
+                  ],
+                },
+                {
+                  'component': 'Column',
+                  'props': {
+                    'size': 'is1',
+                  },
+                },
+                ],
+              }, ],
+            },  ],
+          }, ],
+        },
+        'resources': {
+          // crondata: `${reactadmin.manifest_prefix}cron/view/:id?format=json`,
+        },
+        'onFinish': 'render',
+      },
+    },
+  };
+};
