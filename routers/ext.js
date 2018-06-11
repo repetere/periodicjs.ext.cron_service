d53786d65e90278abf09d4e98494ed879de96a64
yaw joseph etse <yaw.etse@gmail.com>
2018-06-11 06:51:23 -0400

    adding cron basic admin views #2

---
+++ routers/ext.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const periodic = require('periodicjs');
+const extRouter = periodic.express.Router();
+const usesBasicAdmin = periodic.extensions.has('periodicjs.ext.admin');
+const usesReactApp = periodic.extensions.has('periodicjs.ext.reactapp');
+// const controllers = require('../controllers');
+//controllers.admin.adminResLocals
+
+if (usesBasicAdmin || usesReactApp) {
+  const utilities = require('../utilities');
+  const dataRouters = utilities.data.getDataCoreController();
+  
+  if (usesBasicAdmin) {
+    const adminControllers = periodic.controllers.extension.get('periodicjs.ext.admin').admin;
+    extRouter.use(adminControllers.adminResLocals);
+  }
+  extRouter.use(dataRouters.get('standard_cron').router);
+}
+
+module.exports = extRouter;
--- routers/index.js
+++ routers/index.js
@@ -3,7 +3,9 @@
 const periodic = require('periodicjs');
 const extensionRouter = periodic.express.Router();
 const cronRouter = require('./cron');
+const extRouter = require('./ext');
 
+extensionRouter.use('/b-admin/ext/cron_service', extRouter);
 extensionRouter.use('/extension', cronRouter);
 
 module.exports = extensionRouter;
